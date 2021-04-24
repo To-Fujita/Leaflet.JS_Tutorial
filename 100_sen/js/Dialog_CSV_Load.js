2107c53a92604bef2aa09ee10c4c00db4b56af9e
T. Fujita <53250813+To-Fujita@users.noreply.github.com>
2021-04-24 13:56:50 +0900

    Add files via upload

---
+++ 100_sen/js/Dialog_CSV_Load.js
@@ -0,0 +1,73 @@
+﻿// Dialog_CSV_Load.js	2021/4/24 by T. Fujita
+// Usage:
+// var Temp_Data = new Array();
+// var CSV_Data = new Array();
+// Dialog_CSV_Load();
+
+
+$(document).ready( function() {
+        $("body").append('<div id="dialog_CSV_Load" style="z-index: 2000;"><center>'+
+'<p><input type="file" name="select_CSV" id="select_CSV" value=""></p><HR>'+
+'<table border="1" id="dsp_CSV"></table></center></div>');
+
+	$('#dialog_CSV_Load').dialog({
+		autoOpen: false,
+		title: 'CSVファイルを選択してください。',
+		height: 400,
+		width: 500,
+		closeOnEscape: true,
+		modal: true,
+		show: "fade",
+		hide: "fade",
+		buttons: {
+			"OK": function(){
+				Temp_Data = new Array();
+				Temp_Data = CSV_Data;
+				$(this).dialog('close');
+			}
+		}
+	});
+});
+
+function Dialog_CSV_Load() {
+	CSV_Data_Load();
+	$('#dialog_CSV_Load').dialog('open');
+}
+
+function CSV_Data_Load() {
+    if(window.File) {
+		var select = document.getElementById('select_CSV');
+		var output_csv = document.getElementById('dsp_CSV');
+
+		select.addEventListener('change', function(e) {
+			var fileData = e.target.files[0];
+			var reader = new FileReader();
+			reader.onerror = function() {
+				alert('ファイル読み取りに失敗しました')
+			}
+			reader.onload = function(event) {
+				CSV_Data = new Array();
+				var tmp = new Uint8Array(event.target.result);
+				var tmp_text = Encoding.convert(tmp, 'UNICODE', 'AUTO');
+		  		var txtArray = Encoding.codeToString(tmp_text);
+		  		var tmp_head = txtArray.slice(0,1);
+				var lineArr = txtArray.split('\n');
+				for (var i = 0; i < lineArr.length; i++) {
+					CSV_Data[i] = lineArr[i].split(',');
+		  		}
+
+				var insertElement = '';
+				for (var i=0; i<CSV_Data.length; i++) {
+					insertElement += '<tr>';
+					for (var j=0; j<CSV_Data[0].length; j++) {
+						insertElement += '<td>' + CSV_Data[i][j] + '</td>';
+					}
+					insertElement += '</tr>';
+				}
+				output_csv.innerHTML = insertElement;
+
+			}
+			reader.readAsArrayBuffer(fileData);
+		}, false);
+	}
+}
---
+++ 100_sen/js/Dialog_Information.js
@@ -0,0 +1,39 @@
+﻿// Dialog_Information.js	2021/4/24 by T. Fujita
+// Usage:
+// Dialog_Information();
+
+
+$(document).ready( function() {
+        $("body").append('<div id="dialog_Information" style="z-index: 2000;">'+
+'<BR>Excel等で次によりデータを入力し、CSV形式で保存して下さい。<BR>'+
+'A列1行: データのタイトル<BR>'+
+'A列: 番号、記号、日付等<BR>'+
+'B列: 場所の名称<BR>'+
+'C列: 緯度（画面の右下にマウス位置の緯度・経度が表示されます、その値を入力）<BR>'+
+'D列: 経度（　同上　）<BR>'+
+'E列: 所在地等（なくても良い）<BR>'+
+'F列: リンク先（なくても良い）<BR>'+
+'G列: 備考等（表示されません）<BR>'+
+'<BR>次にサンプルを示します。'+
+'<img src="./data/CSV_Data.jpg" width="98%"></div>');
+
+	$('#dialog_Information').dialog({
+		autoOpen: false,
+		title: '個人用CSVファイル作成方法',
+		height: 400,
+		width: 500,
+		closeOnEscape: true,
+		modal: true,
+		show: "fade",
+		hide: "fade",
+		buttons: {
+			"OK": function(){
+				$(this).dialog('close');
+			}
+		}
+	});
+});
+
+function Dialog_Information() {
+	$('#dialog_Information').dialog('open');
+}
